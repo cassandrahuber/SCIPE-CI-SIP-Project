c180d0c3474d1d83aa3cdb040c2d9b5973b6af15
Cassie Huber <chuber@hawaii.edu>
2025-06-13 02:03:03 -0700

    cleaning and adding data

--- working_on.py
+++ working_on.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# clean air quality data
+
 df = pd.read_csv('raw_data/annual_aqi_by_county_2017.csv', 
                  index_col=0,
                  parse_dates=True,
@@ -17,15 +19,33 @@ aqi_df = []
 aqi_files_start_year = 2017
 
 for i in range(5) :
-    # Load the data into a DataFrame
+    # load the data into a DataFrame
     df = pd.read_csv('raw_data/annual_aqi_by_county_' + str(i + aqi_files_start_year) + '.csv')
+    
     df = df[df['State'] == 'California'].drop(columns=['State'])
     df.set_index('County', inplace=True)
+    #print(df.info())
+    #df.set_index('County', inplace=True)
+
+    ##add missing county check
+    ##figure out which counties missing?
+
     aqi_df.append(df)
 
 # combine all aqi years dataframes into one
 combined_aqi_df = pd.concat(aqi_df, ignore_index=True)
 
-#, axis=1
-
 combined_aqi_df.to_csv('processed_data/combined_aqi_data.csv')
+
+# clean asthma emergency department visits data
+
+asthma_df = []
+df = pd.read_excel('raw_data/Asthma_Emergency_2017.xlsx')
+print(df.info())
+
+##figure out what do to abt 'NA' in sheet
+##remove lower and upper limit collumns
+##change "Counties" column to "County"
+##add years column + year data
+##remove california row, add to list for extra data?
+##combine together all excel sheets to one
