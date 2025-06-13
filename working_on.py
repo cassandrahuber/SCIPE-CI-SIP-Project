12966ea17b8029bec2fa8953484b9b4afc9881e0
Cassie Huber <chuber@hawaii.edu>
2025-06-13 14:59:54 -0700

    cleaning

--- working_on.py
+++ working_on.py
@@ -19,13 +19,14 @@ aqi_df = []
 aqi_files_start_year = 2017
 
 for i in range(5) :
-    # load the data into a DataFrame
+    # load the data into a dataframe
     df = pd.read_csv('raw_data/annual_aqi_by_county_' + str(i + aqi_files_start_year) + '.csv')
-    
+    #print(df.info())
+
+    # filter only California data and clean up the dataframe
     df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
     df = df[df['state'] == 'California'].drop(columns=['state'])
     #df.set_index('County', inplace=True)
-    #print(df.info())
 
     aqi_df.append(df)
 
@@ -43,24 +44,49 @@ print(year_counts.sort_values())
 
 
 
+
 # clean asthma emergency department visits data
 
-asthma_df = []
 
 df = pd.read_excel('raw_data/Asthma_Emergency_2017.xlsx')
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 df = df.drop(columns=['lower_95%_limit', 'upper_95%_limit'])    # optional, but cleaner
 df.rename(columns={'counties': 'county'}, inplace=True)
 df['year'] = 2017
-print(df.info())
-asthma_df.append(df)
+#print(df.info())
 
-print(asthma_df[0])
+
+
+asthma_df = []
+asthma_files_start_year = 2017
+
+for i in range(5) :
+    # load the data into a dataframe
+    df = pd.read_excel('raw_data/Asthma_Emergency_' + str(i + asthma_files_start_year) + '.xlsx')
+    #print(df.info())
+
+    # clean up the dataframe
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+    df = df.drop(columns=['lower_95%_limit', 'upper_95%_limit'])  # optional, but cleaner
+    df.rename(columns={'counties': 'county'}, inplace=True)
+ 
+    # add year column and reorder columns to have year after county (like in aqi_df)
+    df['year'] = i + asthma_files_start_year
+    columns_order = ['county', 'year'] + [col for col in df.columns if col not in ['county', 'year']]
+    df = df[columns_order]
+
+    asthma_df.append(df)
+
+# combine all asthma years dataframes into one
+combined_asthma_df = pd.concat(asthma_df, ignore_index=True)
+print(combined_asthma_df)
+combined_asthma_df.to_csv('processed_data/combined_asthma_data.csv')
+
+
+
 
 
 ##remove lower and upper limit collumns
-##change "Counties" column to "County"
-##add years column + year data
 ##remove california row, add to list for extra data?
 ##combine together all excel sheets to one
 ##figure out what do to abt 'NA' in sheet
