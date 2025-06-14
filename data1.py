90a13d1af502fec473cdd91a0e2183633dc56235
Cassie Huber <chuber@hawaii.edu>
2025-06-13 21:59:47 -0700

    cleaning

--- data1.py
+++ data1.py
@@ -10,3 +10,24 @@ df = pd.read_csv('raw_data/annual_aqi_by_county_2017.csv',
 df = df[df['State'] == 'California'].drop(columns=['State'])
 
 df.set_index('County', inplace=True)
+
+##
+
+aqi_df = []
+aqi_files_start_year = 2017
+
+for i in range(5) :
+    # Load the data into a DataFrame
+    df = pd.read_csv('raw_data/annual_aqi_by_county_' + str(i + aqi_files_start_year) + '.csv',
+                     index_col=0,
+                     parse_dates=True,
+                     encoding='utf-8',
+                     low_memory=False)
+    df = df[df['State'] == 'California'].drop(columns=['State'])
+    df.set_index('County', inplace=True)
+    aqi_df.append(df)
+
+# combine all aqi years dataframes into one
+aqi_df = pd.concat(aqi_df)
+
+#, axis=1
