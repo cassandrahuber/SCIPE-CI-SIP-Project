90ae40ca033cfb1154ae3b33c9baf1d8a80a77ea
Cassie Huber <chuber@hawaii.edu>
2025-06-18 23:05:06 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,63 +1,56 @@
 import pandas as pd
 
-
 # clean air quality data:
+def clean_aqi_quality_data(start_year, num_years, input_folder) :
+    aqi_df = []
+    for i in range(num_years) :
+        # load the data into a dataframe
+        df = pd.read_csv(f'{input_folder}/annual_aqi_by_county_{start_year + i}.csv')
 
-aqi_df = []
-aqi_files_start_year = 2017
+        # filter only California data and clean up the dataframe
+        df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+        df = df[df['state'] == 'California'].drop(columns=['state'])
 
-for i in range(5) :
-    # load the data into a dataframe
-    df = pd.read_csv('raw_data/annual_aqi_by_county_' + str(i + aqi_files_start_year) + '.csv')
-    #print(df.info())
+        aqi_df.append(df)
 
-    # filter only California data and clean up the dataframe
-    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
-    df = df[df['state'] == 'California'].drop(columns=['state'])
+    # combine all aqi years dataframes into one
+    cleaned_aqi_df = pd.concat(aqi_df, ignore_index=True)
+    #print(cleaned_aqi_df)
+    #cleaned_aqi_df.to_csv('processed_data/cleaned_aqi.csv')
 
-    aqi_df.append(df)
-
-# combine all aqi years dataframes into one
-cleaned_aqi_df = pd.concat(aqi_df, ignore_index=True)
-#print(cleaned_aqi_df)
-cleaned_aqi_df.to_csv('processed_data/cleaned_aqi.csv')
-
-
+    return cleaned_aqi_df
 
 
 
 
 
 # clean asthma emergency department visits data
+def clean_asthma_ed_visits_data(start_year, num_years, input_folder) :
+    asthma_df = []
+    for i in range(num_years) :
+        # load the data into a dataframe
+        df = pd.read_excel(f'{input_folder}/Asthma_Emergency_{start_year + i}.xlsx')
 
-asthma_df = []
-asthma_files_start_year = 2017
+        # clean up the dataframe
+        df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+        df.rename(columns={'counties': 'county'}, inplace=True)
+        df = df[df['county'] != 'California'] # remove rows with 'California' in county name
+        df = df.drop(columns=['lower_95%_limit', 'upper_95%_limit'])  # optional, but cleaner
 
-for i in range(5) :
-    # load the data into a dataframe
-    df = pd.read_excel('raw_data/Asthma_Emergency_' + str(i + asthma_files_start_year) + '.xlsx')
-    #print(df.info())
+        # add year column and reorder columns to have year after county (like in aqi_df)
+        df['year'] = start_year + i
+        columns_order = ['county', 'year'] + [col for col in df.columns if col not in ['county', 'year']]
+        df = df[columns_order]
 
-    # clean up the dataframe
-    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
-    df.rename(columns={'counties': 'county'}, inplace=True)
-    df = df[df['county'] != 'California'] # remove rows with 'California' in county name
-    df = df.drop(columns=['lower_95%_limit', 'upper_95%_limit'])  # optional, but cleaner
+        asthma_df.append(df)
 
- 
-    # add year column and reorder columns to have year after county (like in aqi_df)
-    df['year'] = i + asthma_files_start_year
-    columns_order = ['county', 'year'] + [col for col in df.columns if col not in ['county', 'year']]
-    df = df[columns_order]
+    # combine all asthma years dataframes into one
+    cleaned_asthma_df = pd.concat(asthma_df, ignore_index=True)
+    #print(cleaned_asthma_df)
+    #cleaned_asthma_df.to_csv('processed_data/cleaned_asthma.csv')
+    #cleaned_asthma_df.to_csv('processed_data/cleaned_asthma_' + str(asthma_files_start_year) + '-' + str(i + asthma_files_start_year) + '.csv')
 
-    asthma_df.append(df)
-
-# combine all asthma years dataframes into one
-cleaned_asthma_df = pd.concat(asthma_df, ignore_index=True)
-#print(cleaned_asthma_df)
-cleaned_asthma_df.to_csv('processed_data/cleaned_asthma.csv')
-#cleaned_asthma_df.to_csv('processed_data/cleaned_asthma_' + str(asthma_files_start_year) + '-' + str(i + asthma_files_start_year) + '.csv')
-
+    return cleaned_asthma_df
 
 
 
