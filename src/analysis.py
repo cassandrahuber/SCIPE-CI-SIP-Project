59e7a4b19be53b52be320845b1e64535b453c057
Cassie Huber <chuber@hawaii.edu>
2025-06-23 17:01:35 -0700

    OLS regression model

--- src/analysis.py
+++ src/analysis.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import statsmodels.formula.api as smf
 
-# 1) Load your merged file
+# load clean merged data
 df = pd.read_csv("../processed_data/merged_data_2017-2021.csv")
 
-# 2) Drop that auto‐generated index column
+# drop auto‐generated index column
 df = df.drop(columns=["Unnamed: 0"])
 
-# "only aqi matters" ~ simple regression
-# 3) Fit an OLS model: asthma_rate ~ median_aqi
-model = smf.ols("asthma_rate ~ median_aqi", data=df).fit()
-print(model.summary())
+# simple OLS regression model
+# note: this fit does not account for the whole strength of the relationship
 
-###########
+# "only aqi vs asthma rate"
+#model = smf.ols("asthma_rate ~ median_aqi", data=df).fit()
+#print(model.summary())
 
-import statsmodels.formula.api as smf
 
+# multiple OLS regression model
+# note: more complex model but gave much more stronger relationship between aqi and asthma rate
+
+# "aqi vs asthma rate by county and year"
 mod = smf.ols('asthma_rate ~ median_aqi + C(county) + C(year)', data=df).fit()
-mod.summary()
-#print(mod.summary())
+print(mod.summary())
